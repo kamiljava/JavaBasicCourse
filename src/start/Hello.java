50be2083a80d9763dd89080173556ad13ba29691
KamilJava <kamillapka1@gmail.com>
2019-02-14 11:08:50 +0100

    qw :qw
    i#
    
    :
    iPlease enter the commit message for your changes. Lines starting

--- src/start/Hello.java
+++ src/start/Hello.java
@@ -1,7 +1,11 @@
 package start;
 
 import javax.swing.*;
-
+// Komentarz
+// psvm - public static void main(String[] args)
+// sout - System.out.println();
+// CTRL + SHIFT + F10 - run
+//CTRL + D Duplikowanie
 public class Hello {
 
     public static void main(String[] args) {
@@ -9,6 +13,30 @@ public class Hello {
         //System.out.println("Hello World"+ i);}
         //System.out.println("Skrót sout");
         //JOptionPane.showMessageDialog(null ,"Hello World");
-        System.out.println(System.getProperty("java.runtime.version"));
+        //System.out.println(System.getProperty("java.runtime.version"));
+        int lunchTime = 12345;
+        int endTime= -12345667;
+
+        System.out.println(lunchTime);
+        System.out.println(endTime);
+
+        double salary = 2100.99;
+        System.out.println(salary);
+
+        char signA = 'A';
+        char sign9 = '9';
+        char sign= '\'';
+        char signspace= ' ';
+        System.out.println(signA);
+        System.out.println(sign9);
+        System.out.println(sign);
+        System.out.println(signspace);
+
+        boolean flag = true;
+        boolean isLounch = false;
+
+        System.out.println(flag);
+        System.out.println(isLounch);
+
     }
 }
