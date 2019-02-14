bb9f9c7f40b2d6780182584194ba9d009f0130c8
KamilJava <kamillapka1@gmail.com>
2019-02-14 10:26:16 +0100

    start

---
+++ src/start/Hello.java
@@ -0,0 +1,14 @@
+package start;
+
+import javax.swing.*;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        //for (int i = 0; i < 10; i++){
+        //System.out.println("Hello World"+ i);}
+        //System.out.println("Skrót sout");
+        //JOptionPane.showMessageDialog(null ,"Hello World");
+        System.out.println(System.getProperty("java.runtime.version"));
+    }
+}
