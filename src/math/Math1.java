1da1d147101373d4fcf9c7c176b9697c8c260404
KamilJava <kamillapka1@gmail.com>
2019-02-14 16:19:12 +0100

    Aktualizacja7

--- src/math/Math1.java
+++ src/math/Math1.java
@@ -1,13 +1,19 @@
 package math;
 
+import java.util.Scanner;
+
 import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 
 public class Math1 {
     public static void main(String[] args) {
 
-        int a = 5;
-        int b = 3;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj wartosć zmiennej a: ");
+        int a = scanner.nextInt();
+        System.out.println("Podaj wartosć zmiennej b: ");
+        int b = scanner.nextInt();
+        //scanner.close();
 
         System.out.println(0.5*a*(a *sqrt(3)/2));
 
@@ -19,8 +25,9 @@ public class Math1 {
         System.out.println((double) (grade1 + grade2 + grade3)/3);
 
         System.out.println(a*b);
-
-        double salary_net = 8000;
+        System.out.println("Podaj kwotę netto: ");
+        double salary_net = scanner.nextDouble();
+        scanner.close();
         int vat_tax = 23;
 
         System.out.println(salary_net * (1 + (double)vat_tax/100));
