fe772dc3c904bac5893bf43ac4e3559d36bb9dfd
KamilJava <kamillapka1@gmail.com>
2019-02-19 11:57:46 +0100

    datetime sortowanie

--- src/OOPbasic/UserMain.java
+++ src/OOPbasic/UserMain.java
@@ -4,14 +4,24 @@ import controller.UserController;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class UserMain {
     public static void main(String[] args) {
         UserController uc = new UserController();
-        uc.addUser("Janusz","Kowalski","tojajanusz","pass123", "e@e.pl","55", LocalDate.of(2000,01, 13), LocalDateTime.now());
-        uc.addUser("Grażyna","kowalska","tojagrazyna","pass123", "e@e.pl","55", LocalDate.of(2010,02, 01), LocalDateTime.now());
-        uc.addUser("Brajan","Kowalski","brajanek","pass123", "e@e.pl","55", LocalDate.of(1988,03, 20), LocalDateTime.now());
-        uc.addUser("Jesica","Kowalska","jessi","pass123", "e@e.pl","55", LocalDate.of(1960,04, 25), LocalDateTime.now());
+        uc.addUser("Janusz","Kowalski","tojajanusz","pass123", "eeee@e.pl","222355", LocalDate.of(2000,01, 13), LocalDateTime.now());
+        uc.addUser("Grażyna","kowalska","tojagrazyna","pass123", "gg@e.pl","6665", LocalDate.of(2010,02, 01), LocalDateTime.now());
+        uc.addUser("Brajan","Kowalski","brajanek","pass123", "vvv@e.pl","566675", LocalDate.of(1988,03, 20), LocalDateTime.now());
+        uc.addUser("Jesica","Kowalska","jessi","pass123", "nnn@e.pl","88855", LocalDate.of(1965,04, 25), LocalDateTime.now());
+        uc.addUser("Jesica","Kowalska","jessi","pass123", "nnn@e.pl","88855", LocalDate.of(1920,05, 25), LocalDateTime.now());
+
+        uc.getAllUser();
+        System.out.println("Test Date");
+        System.out.println("User pierwszy: " + uc.birthDateComparator(0,1) );
+        System.out.println("User drugi: " + uc.birthDateComparator(1,2) );
+
+        System.out.println("Posortowane po dacie: ASC");
+        uc.sortByDate();
         uc.getAllUser();
     }
 }
--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -6,18 +6,52 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class UserController {
-    User users [] = new User[100];
+    User users[] = new User[100];
     int index;
-    public void  addUser(String name, String lastname, String login, String password, String email, String phone, LocalDate birth_date, LocalDateTime registration_date_time){
-        User u = new User(name,lastname,login,password,email,phone,birth_date,registration_date_time);
-       // System.out.println(u);
+
+    public void addUser(String name, String lastname, String login, String password, String email, String phone, LocalDate birth_date, LocalDateTime registration_date_time) {
+        User u = new User(name, lastname, login, password, email, phone, birth_date, registration_date_time);
+        //dodawanie godziny+ do czasu aktualnego
+        u.setRegistration_date_time(u.getRegistration_date_time().plusHours(1));
+        //
         users[index] = u;
         index++;
     }
-    public  void  getAllUser(){
 
-        for (int i =0 ; i<index; i++){
+    public void getAllUser() {
+
+        for (int i = 0; i < index; i++) {
             System.out.println(users[i]);
+        }
+    }
+    //metoda porownujaca daty urodzenie dwoch uzytkownikow
+    //jezeli pierwza dat jest <od drugiej--> true
+    //w przeciwnym razie --> false
+
+    public boolean birthDateComparator(User u1, User u2) {
+        return u1.getBirth_date().isBefore(u2.getBirth_date());
+    }
+
+    public boolean birthDateComparator(int index1, int index2) {
+        return users[index1].getBirth_date().isBefore(users[index2].getBirth_date());
+    }
+
+    public void sortByDate() {
+        for (int j = 0; j < index; j++) {
+            for (int i = 1; i < index; i++) {
+                //sprawdzanie czy pierwszy jest mlodszy od pierwszego
+                if (birthDateComparator(i - 1, i)) {
+
+                    //tymczasowy element
+                    User u1 = users[i - 1];
+                    //tymczasowy element
+                    User u2 = users[i];
+                    //zamiana miejscami w komorkach tablicy
+                    users[i - 1] = u2;
+                    users[i] = u1;
+
+                }
+            }
         }
 
     }
