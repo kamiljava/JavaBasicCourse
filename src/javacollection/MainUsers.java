16f5eaf74d1cd6f775af93765c449ccbb78013c3
KamilJava <kamillapka1@gmail.com>
2019-02-21 14:22:24 +0100

    kolekcje3

--- src/javacollection/MainUsers.java
+++ src/javacollection/MainUsers.java
@@ -7,11 +7,16 @@ public class MainUsers {
     public static void main(String[] args) {
         UserController uc = new UserController();
         uc.addUser("Janusz","11");
-        uc.addUser("bb","22");
-        uc.addUser("nn","33");
-        uc.addUser("dd","44");
+        uc.addUser("Kamil","22");
+        uc.addUser("Karol","33");
+        uc.addUser("Michał","44");
         uc.addUser("Janusz","22");
-
+        uc.deleteUser("Karol");
+        uc.deleteUser("Grażyna");
+        uc.showUsers();
+        uc.changePassword("Kamil","22","88","88");
+        uc.changePassword("Andrzej","22","88","88");
+        uc.showUsers();
 
     }
 }
--- src/javacollection/controller/UserController.java
+++ src/javacollection/controller/UserController.java
@@ -12,22 +12,51 @@ public class UserController {
     List<User> registered_user = new ArrayList<>();
     Set<String> registered_login = new HashSet<>();
 
-    public boolean addUser(String login, String password){
-        if (registered_login.contains(login)){
+    public boolean addUser(String login, String password) {
+        if (registered_login.contains(login)) {
             System.out.println("Login " + login + " istnieje w bazie");
             return false;
         }
         User u = new User(login, password);
         registered_user.add(u);
         registered_login.add(login);
-        System.out.println("Zarejestrowano użytkownika");
+        System.out.println("Zarejestrowano użytkownika o loginie " + login);
         return true;
     }
 
-    public void showUsers(){
+    public void showUsers() {
         registered_user.forEach(user -> System.out.println(user));
-
 
     }
 
+    public boolean deleteUser(String loginDelete) {
+
+        for (int i = 0; i < registered_user.size(); i++) {
+            if (registered_user.get(i).getLogin().equals(loginDelete)) {
+                registered_user.remove(registered_user.get(i));
+                registered_login.remove(loginDelete);
+                System.out.println("Usunieto uzytkownika: " + loginDelete);
+                return true;
+            }
+
+        }
+        System.out.println("Nie ma takiego uzytkownia " + loginDelete);
+        return false;
+    }
+
+    public boolean changePassword(String login, String oldPassword, String newPassword1, String newPassword2) {
+        for (int i = 0; i < registered_user.size(); i++) {
+            if (registered_user.get(i).getLogin().equals(login)) {
+                if (newPassword1.equals(newPassword2) && !newPassword1.equals(oldPassword)) {
+                    registered_user.get(i).setPassword(newPassword1);
+                    System.out.println("Hasło użytkownika " + login + " zostało zmienione");
+                    return true;
+                }
+                System.out.println("Zle podano dane");
+                return false;
+            }
+        }
+        System.out.println("Użytkownik: " + login + " nie istnieje lub podałeś złe hasło");
+        return false;
+    }
 }
